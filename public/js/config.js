c58aae569efa2236788021c780c1399651de5816
Pepe Sufragio <albertkstyon@gmail.com>
2026-03-23 18:58:47 +0100

    firebase init

---
+++ public/js/config.js
@@ -0,0 +1,10 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyCvLdrwhHZDcSnpNAu9QX1K3w5Uyq9jXWg",
+    authDomain: "items-9da8e.firebaseapp.com",
+    projectId: "items-9da8e",
+    storageBucket: "items-9da8e.firebasestorage.app",
+    messagingSenderId: "251187970133",
+    appId: "1:251187970133:web:51db2cf71468483cd594dc"
+});
+
+const auth = firebase.auth();
